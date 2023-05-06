635d03909f18981374025f1101b9127285aee3c2
Slobodan Milosevic <milosevicslobodan@hotmail.com>
2023-05-06 13:51:12 +0200

    Update serach header

--- src/components/SearchHeader.js
+++ src/components/SearchHeader.js
@@ -1,7 +1,43 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import SearchBox from "@/components/SearchBox";
+import { RiSettings3Line } from "react-icons/ri";
+import { TbGridDots } from "react-icons/tb";
+import SearchHeaderOptions from "@/components/SearchHeaderOptions";
 
 function SearchHeader() {
-  return <div>Search header</div>;
+  return (
+    <header className={"sticky top-0 bg-white"}>
+      <div className={"flex w-full p-6 items-center justify-between"}>
+        <Link href={"/"}>
+          <Image
+            height={"40"}
+            width={"120"}
+            src={
+              "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/800px-Google_2015_logo.svg.png"
+            }
+            alt={"Google logo"}
+          />
+        </Link>
+        <div className={"flex-1"}>
+          <SearchBox />
+        </div>
+        <div className={"hidden md:inline-flex space-x-2 "}>
+          <RiSettings3Line className={"header-icon"} />
+          <TbGridDots className={"header-icon"} />
+        </div>
+        <button
+          className={
+            "bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-all ml-2"
+          }
+        >
+          Sign in
+        </button>
+      </div>
+      <SearchHeaderOptions />
+    </header>
+  );
 }
 
 export default SearchHeader;
---
+++ src/components/SearchHeaderOptions.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SearchHeaderOptions() {
+  return <div>Option</div>;
+}
+
+export default SearchHeaderOptions;
