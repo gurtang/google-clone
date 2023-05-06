4106e3fb9359ebedb00a927016f47452292f6f59
Slobodan Milosevic <milosevicslobodan@hotmail.com>
2023-05-06 15:37:17 +0200

    Fetch data from google search api

--- src/app/search/web/page.js
+++ src/app/search/web/page.js
@@ -1,7 +1,12 @@
 import React from "react";
 
-function WebSearchPage() {
-  return <div>Web search page</div>;
+async function WebSearchPage({ searchParams }) {
+  const address = `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`;
+  const response = await fetch(address);
+  const data = await response.json();
+  const results = data.items;
+
+  return <>{results && results.map((result) => <h1>{result.title}</h1>)}</>;
 }
 
 export default WebSearchPage;
