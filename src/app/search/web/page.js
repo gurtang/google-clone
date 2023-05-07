b3f83f35ae287773fe88f5852c789493969abb91
Slobodan Milosevic <milosevicslobodan@hotmail.com>
2023-05-07 14:34:14 +0200

    Handle possible errors

--- src/app/search/web/page.js
+++ src/app/search/web/page.js
@@ -1,11 +1,29 @@
 import React from "react";
+import Link from "next/link";
 
 async function WebSearchPage({ searchParams }) {
   const address = `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`;
   const response = await fetch(address);
+
+  if (!response.ok) {
+    throw new Error("Something went wrong");
+  }
   const data = await response.json();
   const results = data.items;
 
+  if (!results) {
+    return (
+      <div className={"flex flex-col justify-center items-center pt-10 "}>
+        <h1 className={"text-3xl mb-4"}>No results found</h1>
+        <p className={"text-lg"}>
+          Try searching for something else or go back to the homepage.
+        </p>
+        <Link className={"text-blue-500"} href={"/"}>
+          Home
+        </Link>
+      </div>
+    );
+  }
   return <>{results && results.map((result) => <h1>{result.title}</h1>)}</>;
 }
 
